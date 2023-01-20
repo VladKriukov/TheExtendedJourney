9aa90ea1f45d4f739f8d8f58fb9e173fd5892474
VladKriukov <vladik.kriukov@gmail.com>
2023-01-20 12:30:12 +0000

    Mostly implemented test AI

--- ZombieSurvival/Assets/Scripts/AnimalAI.cs
+++ ZombieSurvival/Assets/Scripts/AnimalAI.cs
@@ -7,9 +7,10 @@ public class AnimalAI : MonoBehaviour
         Idle, Avoiding, Roaming, Attacking
     }
     public MovementType movementType;
-
     MovementType prevMovementType;
 
+    public Transform chaseTarget;
+    [SerializeField] float losingSightDistance;
     [SerializeField] Vector3 destinationPoint;
     [SerializeField] float roamingDistance;
     [SerializeField] float destinationStoppingThreshold;
@@ -19,15 +20,28 @@ public class AnimalAI : MonoBehaviour
     [SerializeField] float runSpeed;
     [SerializeField] float rotationSpeed;
     [SerializeField] float avoidanceTime;
+    [SerializeField] LayerMask layerMask;
 
     Rigidbody rb;
     bool avoidanceDirection;
     float idleTime;
     float currentSpeed;
 
+    RaycastHit hit;
+    bool hitting;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            chaseTarget = other.gameObject.transform;
+            destinationPoint = chaseTarget.position;
+        }
     }
 
     public void AvoidObstacle(bool right)
@@ -39,17 +53,30 @@ public class AnimalAI : MonoBehaviour
 
     public void ResumeMovement()
     {
-        movementType = prevMovementType;
+        movementType = MovementType.Roaming;
     }
 
     private void Update()
     {
         if (destinationPoint != Vector3.zero)
         {
-            rb.velocity = transform.forward * currentSpeed;
+            if (chaseTarget != null && Vector3.Distance(transform.position, chaseTarget.position) < losingSightDistance)
+            {
+                movementType = MovementType.Attacking;
+                destinationPoint = chaseTarget.position;
+            }
+            else
+            {
+                movementType = MovementType.Roaming;
+                chaseTarget = null;
+            }
+            rb.velocity = transform.forward * currentSpeed + Vector3.down;
+            //rb.velocity += Vector3.down * 2f;
 
-            Debug.Log("Angle: " + Vector3.Angle(transform.forward, destinationPoint));
-            Debug.Log("Dot: " + Vector3.Dot(transform.position, destinationPoint));
+            Vector3 targetDirection = destinationPoint - transform.position;
+            float singleStep = rotationSpeed * Time.deltaTime;
+            Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+            Debug.DrawRay(transform.position, newDirection, Color.red);
 
             switch (movementType)
             {
@@ -58,20 +85,29 @@ public class AnimalAI : MonoBehaviour
                     break;
                 case MovementType.Roaming:
                     currentSpeed = walkSpeed;
-                    rb.angularVelocity = new Vector3(0, Vector3.Dot(transform.right, destinationPoint), 0);
+                    transform.rotation = Quaternion.LookRotation(newDirection);
+                    //transform.rotation = new Quaternion(0, transform.rotation.y, 0, 1);
                     break;
                 case MovementType.Attacking:
                     currentSpeed = runSpeed;
-                    rb.angularVelocity = new Vector3(0, Vector3.Angle(transform.forward, destinationPoint), 0);
+                    transform.rotation = Quaternion.LookRotation(newDirection);
+                    //transform.rotation = new Quaternion(0, transform.rotation.y, 0, 1);
                     break;
                 default:
                     break;
             }
             if (Vector3.Distance(transform.position, destinationPoint) <= destinationStoppingThreshold)
             {
-                idleTime = Random.Range(minIdleTime, maxIdleTime);
-                movementType = MovementType.Idle;
-                destinationPoint = Vector3.zero;
+                if (movementType == MovementType.Roaming)
+                {
+                    idleTime = Random.Range(minIdleTime, maxIdleTime);
+                    movementType = MovementType.Idle;
+                    destinationPoint = Vector3.zero;
+                }
+                else
+                {
+                    // attack
+                }
             }
         }
         else if (movementType == MovementType.Idle)
@@ -82,11 +118,21 @@ public class AnimalAI : MonoBehaviour
                 CreateRandomDestination();
             }
         }
+        else
+        {
+            CreateRandomDestination();
+        }
     }
 
+    // todo: fix so it doesn't go in the water (convert to IEnumerator)
     void CreateRandomDestination()
     {
         destinationPoint = new Vector3(transform.position.x + Random.Range(0, roamingDistance), transform.position.y, transform.position.z + Random.Range(0, roamingDistance));
+        hitting = Physics.Raycast(destinationPoint + new Vector3(0, 50, 0), Vector3.down, out hit, 200, layerMask);
+        if (hitting == true)
+        {
+            destinationPoint = new Vector3(destinationPoint.x, hit.point.y, destinationPoint.z);
+        }
         movementType = MovementType.Roaming;
     }
 
