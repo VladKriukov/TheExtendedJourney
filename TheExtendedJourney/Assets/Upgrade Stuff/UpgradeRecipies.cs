62511a9deb7a51ce086b16b8d0d06c16bb9e7e72
JamesGriffiths1994 <82873886+JamesGriffiths1994@users.noreply.github.com>
2023-03-24 13:04:37 +0000

    Upgrade System Framework (actually this time)
    Adds the upgrade framework without changing anything in the scene to not mess with anything that was being done

---
+++ TheExtendedJourney/Assets/Upgrade Stuff/UpgradeRecipies.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*[CreateAssetMenu(fileName = "UpgradeRecipe", menuName = "ScriptableObjects/UpgradeRecipes", order = 1)]
+public class UpgradeRecipies : ScriptableObject
+{
+    public List<UpgradeRecipie> recipies= new List<UpgradeRecipie>();
+}
+[Serializable]
+public struct UpgradeRecipie
+{
+    public string upgradeName;
+    public List<RecipeItem> itemNeeded;
+    public int output;
+    //public List<RequiredItem> itemNeeded;
+}
+[Serializable]
+public struct RequiredItem
+{
+    public ItemProperties.ItemType type;
+    public int ammount;
+}*/
